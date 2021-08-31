2ebcd15dfbb4412d87a1ee49288bcfbaf901a7f2
touros91 <andreageraci91@gmail.com>
2021-08-31 17:06:05 +0200

    Versione migliore e commentato quella col while

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -2,56 +2,50 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+function palindroma(parola) {
+    var parolaReverse = "";
+    for (var i = parola.length - 1; i >= 0; i--) {
+            parolaReverse += parola.charAt(i);
 
-function palindroma(parola){
-    var controllo;
-    for (var i = parola.length-1; i > 0; i--) {
-        for (var x = 0; x < parola.length; x++) {
-            if (parola.charAt(i) == parola.charAt(x)) {
-                controllo = true;
-            } else {
-                controllo = false;
-                i = 0;
-            }
-        }
     }
-    if (controllo){
-        alert("La parola è palindroma");
-    } else {
-        alert ("La parola non è palindroma");
-    }
+    return parolaReverse;
 }
 
 var parolaUtente = prompt("Inserisci una parola");
-var risultato = palindroma(parolaUtente);
+var parolaNuova = palindroma(parolaUtente);
 
-
+if (parolaUtente == parolaNuova) {
+    alert("La parola è palindroma");
+} else {
+    alert("La parola non è palindroma");
+}
 
 
 // VARIANTE CON IL CICLO WHILE
 
-// var parola = prompt("Inserisci una parola");
+// function palindroma(parola){
+//     var i = parola.length-1;
+//     var x = 0;
+//     var controllo;
 
-// console.log(parola.length);
-// var i = parola.length-1;
-// var x = 0;
+//     while (i > 0 && x < parola.length) {
+//     // for (var i = parola.length-1; i > 0; i--) {
+//         if (parola.charAt(i) == parola.charAt(x)) {
+//             x++;
+//             i--;
+//             controllo = true;
+//         } else {
+//             controllo = false;
+//             i = 0;
+//         }
+//     }
 
-// var controllo = true;
-
-// while (i > 0 && x < parola.length) {
-// // for (var i = parola.length-1; i > 0; i--) {
-//     if (parola.charAt(i) == parola.charAt(x)) {
-//         x++;
-//         i--;
-//         controllo = true;
+//     if (controllo){
+//         alert("La parola è palindroma");
 //     } else {
-//         controllo = false;
-//         i = 0;
+//         alert("La parola non è palindroma");
 //     }
 // }
 
-// if (controllo){
-//     alert("uguale");
-// } else {
-//     alert ("diverso");
-// }
+// var parolaUtente = prompt("Inserisci una parola");
+// var controllo = palindroma(parolaUtente);
